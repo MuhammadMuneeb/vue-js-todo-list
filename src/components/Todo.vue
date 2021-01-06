26fe1f3cd90ece796d3197adb95d1228165b8203
MuhammadMuneeb <haqgreat@gmail.com>
2021-01-06 16:55:32 +0500

    Initial commit with a simple CRUD

---
+++ src/components/Todo.vue
@@ -0,0 +1,31 @@
+<template>
+    <div :class="{'sax':todo.completed , 'incomplete':!todo.completed}">
+        <p @click="markComplete">{{ todo.title }}</p>
+        <button @click="$emit('delete-todo', todo.id)">Delete</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Todo',
+    props: ['todo'],
+    methods: {
+        markComplete(){
+            this.todo.completed = !this.todo.completed
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+.incomplete {
+    font-size: 30px;
+}
+
+.sax {
+    text-decoration: line-through;
+    font-size: 25px;
+    color: red;
+  }
+</style>
---
+++ src/components/Todos.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <h2>My Todolist</h2>
+        <ul>
+            <li v-for="todo in todos" v-bind:key="todo.id">
+                <Todo v-bind:todo="todo" v-on:delete-todo="$emit('delete-todo', todo.id)"/>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+    import Todo from './Todo';
+    
+
+    export default{
+        name: 'Todos',
+        components: {
+            Todo,
+        },
+        props: [
+            "todos"
+        ]
+    }
+</script>
+<style scoped>
+</style>
